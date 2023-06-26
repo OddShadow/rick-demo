46988684e2198206009246cacf2630ccba829b8a
Rick <mymiaojirui@gmail.com>
2023-06-26 11:19:23 +0800

    demo-juc

--- demo-juc-basic/src/test/java/org/example/DemoTest.java
+++ demo-juc-basic/src/test/java/org/example/DemoTest.java
@@ -1,33 +1,5 @@
 package org.example;
 
-import org.junit.Test;
-
-import java.util.Date;
-
 public class DemoTest {
-    
-    @Test
-    public void test01() {
-        System.out.println("start=====");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("end=====");
-    }
-    
-    @Test
-    public void test02() {
-        // java.lang.IllegalMonitorStateException: current thread is not owner
-        System.out.println("start=====");
-        Date date = new Date();
-        try {
-            date.wait(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("end=====");
-    }
     
 }
